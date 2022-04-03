638efa6086886835e9cc9f38ccdc18fef483083d
Daniel <danielburlacu96@gmail.com>
2022-04-03 20:04:53 +0300

    Entities created!

--- src/main/java/com/magazin/demo/model/Wishlist.java
+++ src/main/java/com/magazin/demo/model/Wishlist.java
@@ -1,4 +1,25 @@
 package com.magazin.demo.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name="wishlist")
+@Getter
+@Setter
 public class Wishlist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User userId;
+
+    @ManyToMany
+    @JoinColumn(name = "products")
+    private Set<Product> products;
 }
